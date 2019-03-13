5a62ee887a6ff84ea1612b2498d92d2da64a3063
Jasmine Walters <jsmnwltrs@gmail.com>
2019-03-12 21:18:58 -0500

    added planet lists

--- Planets-Ships/Program.cs
+++ Planets-Ships/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Planets_Ships
 {
@@ -6,6 +7,26 @@ namespace Planets_Ships
     {
         static void Main(string[] args)
         {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> anotherPlanetList = new List<string>() { "Uranus", "Neptune" };
+
+            planetList.AddRange(anotherPlanetList);
+
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+
+            planetList.Add("Pluto");
+
+            List<string> rockyPlanets = new List<string>();
+
+            rockyPlanets = planetList.GetRange(0, 4);
+
+            planetList.Remove("Pluto");
+
+            Console.ReadLine();
         }
     }
 }
